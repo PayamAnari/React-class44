f64216c8443e5824ac2d430ec92106db38f02349
PayamAnari <anari.p62@gmail.com>
2023-09-25 15:34:48 +0200

    Add products.js

---
+++ week2/project/ecommerce/src/component/Products.js
@@ -0,0 +1,127 @@
+import React, { useState, useEffect } from 'react';
+import Skeleton from 'react-loading-skeleton';
+import { NavLink } from 'react-router-dom';
+import Buttons from './Buttons';
+
+const Products = () => {
+  const [data, setData] = useState([]);
+  const [filter, setFilter] = useState(data);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    let componentMounted = true;
+
+    const getProducts = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch('http://fakestoreapi.com/products');
+        if (response.ok) {
+          if (componentMounted) {
+            const jsonData = await response.clone().json();
+            setData(jsonData);
+            setFilter(jsonData);
+            setLoading(false);
+          }
+        } else {
+          throw new Error('Error fetching products');
+        }
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+      }
+    };
+
+    getProducts();
+
+    return () => {
+      componentMounted = false;
+    };
+  }, []);
+
+  const Loading = () => {
+    return (
+      <>
+        <div className="col-12 text-center">
+          <h3>Loading...</h3>
+        </div>
+        <div className="col-md-3">
+          <Skeleton height={350} />
+        </div>
+        <div className="col-md-3">
+          <Skeleton height={350} />
+        </div>
+        <div className="col-md-3">
+          <Skeleton height={350} />
+        </div>
+        <div className="col-md-3">
+          <Skeleton height={350} />
+        </div>
+      </>
+    );
+  };
+
+  const filterProducts = (cat) => {
+    const updatedList =
+      cat === 'All' ? data : data.filter((x) => x.category === cat);
+    setFilter(updatedList);
+  };
+
+  const ShowProducts = () => {
+    return (
+      <>
+        <Buttons filterProducts={filterProducts} />
+        {filter.map((product) => {
+          return (
+            <div className="col-md-4 mb-4" key={product.id}>
+              <div className="card h-100 text-center p-4">
+                <img
+                  src={product.image}
+                  className="card-img-top"
+                  alt={product.title}
+                  height="250px"
+                />
+                <div className="card-body">
+                  <h5 className="card-title mb-0">
+                    {product.title.substring(0, 12)} ...
+                  </h5>
+                  <p className="card-text fw-bold">${product.price}</p>
+                  <NavLink
+                    to={`/products/${product.id}`}
+                    className="btn btn-outline-dark"
+                  >
+                    Buy Now
+                  </NavLink>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </>
+    );
+  };
+
+  return (
+    <>
+      <div className="container my-5 py-5">
+        <div className="row">
+          <div className="col-12 mb-5">
+            <h1 className="display-6 fw-bolder text-center">Latest Products</h1>
+            <hr />
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          {loading ? (
+            <Loading />
+          ) : error ? (
+            <div>Error: {error}</div>
+          ) : (
+            <ShowProducts />
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Products;
