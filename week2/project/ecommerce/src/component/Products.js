c687e2e1176654a0dda085f053b04df751f0822b
PayamAnari <anari.p62@gmail.com>
2023-09-30 20:31:08 +0200

    extracting components

--- week2/project/ecommerce/src/component/Products.js
+++ week2/project/ecommerce/src/component/Products.js
@@ -5,23 +5,20 @@ import Buttons from './Buttons';
 
 const Products = () => {
   const [data, setData] = useState([]);
-  const [filter, setFilter] = useState(data);
-  const [loading, setLoading] = useState(false);
+  const [filter, setFilter] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     let componentMounted = true;
 
     const getProducts = async () => {
-      setLoading(true);
       try {
         const response = await fetch('http://fakestoreapi.com/products');
-        if (response.ok) {
-          if (componentMounted) {
-            const jsonData = await response.clone().json();
-            setData(jsonData);
-            setFilter(jsonData);
-          }
+        if (response.ok && componentMounted) {
+          const jsonData = await response.json();
+          setData(jsonData);
+          setFilter(jsonData);
         } else {
           throw new Error(
             'Oops! Something went wrong. Please try again later.',
@@ -32,7 +29,9 @@ const Products = () => {
           'Oops! Unable to fetch products. Please check your internet connection and try again.',
         );
       } finally {
-        setLoading(false);
+        if (componentMounted) {
+          setLoading(false);
+        }
       }
     };
 
@@ -49,18 +48,11 @@ const Products = () => {
         <div className="col-12 text-center">
           <h3>Loading...</h3>
         </div>
-        <div className="col-md-3">
-          <Skeleton height={350} />
-        </div>
-        <div className="col-md-3">
-          <Skeleton height={350} />
-        </div>
-        <div className="col-md-3">
-          <Skeleton height={350} />
-        </div>
-        <div className="col-md-3">
-          <Skeleton height={350} />
-        </div>
+        {[1, 2, 3, 4].map((id) => (
+          <div className="col-md-3" key={id}>
+            <Skeleton height={350} />
+          </div>
+        ))}
       </>
     );
   };
@@ -75,54 +67,50 @@ const Products = () => {
     return (
       <>
         <Buttons filterProducts={filterProducts} />
-        {filter.map((product) => {
-          return (
-            <div className="col-md-4 mb-4" key={product.id}>
-              <div className="card h-100 text-center p-4">
-                <img
-                  src={product.image}
-                  className="card-img-top"
-                  alt={product.title}
-                  height="250px"
-                />
-                <div className="card-body">
-                  <h5 className="card-title mb-2">{product.title}</h5>
-                  <p className="card-text fw-bold">${product.price}</p>
-                  <NavLink
-                    to={`/products/${product.id}`}
-                    className="btn btn-outline-dark"
-                  >
-                    Buy Now
-                  </NavLink>
-                </div>
+        {filter.map((product) => (
+          <div className="col-md-4 mb-4" key={product.id}>
+            <div className="card h-100 text-center p-4">
+              <img
+                src={product.image}
+                className="card-img-top"
+                alt={product.title}
+                height="250px"
+              />
+              <div className="card-body">
+                <h5 className="card-title mb-2">{product.title}</h5>
+                <p className="card-text fw-bold">${product.price}</p>
+                <NavLink
+                  to={`/products/${product.id}`}
+                  className="btn btn-outline-dark"
+                >
+                  Buy Now
+                </NavLink>
               </div>
             </div>
-          );
-        })}
+          </div>
+        ))}
       </>
     );
   };
 
   return (
-    <>
-      <div className="container my-5 py-5">
-        <div className="row">
-          <div className="col-12 mb-5">
-            <h1 className="display-6 fw-bolder text-center">Latest Products</h1>
-            <hr />
-          </div>
-        </div>
-        <div className="row justify-content-center">
-          {loading ? (
-            <Loading />
-          ) : error ? (
-            <div>Error: {error}</div>
-          ) : (
-            <ShowProducts />
-          )}
+    <div className="container my-5 py-5">
+      <div className="row">
+        <div className="col-12 mb-5">
+          <h1 className="display-6 fw-bolder text-center">Latest Products</h1>
+          <hr />
         </div>
       </div>
-    </>
+      <div className="row justify-content-center">
+        {loading ? (
+          <Loading />
+        ) : error ? (
+          <div>Error: {error}</div>
+        ) : (
+          <ShowProducts />
+        )}
+      </div>
+    </div>
   );
 };
 
