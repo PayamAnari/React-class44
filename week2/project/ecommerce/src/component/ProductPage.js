49ae174657e18c56c3f5bcd1f01494324cd4e70b
PayamAnari <anari.p62@gmail.com>
2023-09-30 20:16:49 +0200

    Correction skeleton

--- week2/project/ecommerce/src/component/ProductPage.js
+++ week2/project/ecommerce/src/component/ProductPage.js
@@ -1,37 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { NavLink, useParams } from 'react-router-dom';
-import Skeleton from 'react-loading-skeleton';
 
-const Loading = () => (
-  <>
-    <div className="col-12 text-center">
-      <h3>Loading...</h3>
-    </div>
-    <div className="col-md-6">
-      <Skeleton height={400} />
-    </div>
-    <div className="col-md-6">
-      <Skeleton height={50} width={300} />
-      <Skeleton height={75} />
-      <Skeleton height={25} width={150} />
-      <Skeleton height={75} />
-      <Skeleton height={150} />
-      <Skeleton height={50} width={100} />
-      <Skeleton height={150} width={100} style={{ marginLeft: 6 }} />
-    </div>
-  </>
-);
-
-const ProductPage = () => {
+const ProductCard = () => {
   const { id } = useParams();
-  const [product, setProduct] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [product, setProduct] = useState({});
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const getProduct = async () => {
-      setLoading(true);
-
       try {
         const response = await fetch(`http://fakestoreapi.com/products/${id}`);
         if (response.ok) {
@@ -49,58 +26,49 @@ const ProductPage = () => {
     getProduct();
   }, [id]);
 
+  if (loading) {
+    return (
+      <div className="col-12 text-center">
+        <h3>Loading...</h3>
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
-    <>
-      <div className="container py-5">
-        <div className="row py-3">
-          {loading ? (
-            <Loading />
-          ) : error ? (
-            <div>Error: {error}</div>
-          ) : (
-            <>
-              <div className="col-md-6 pt-5">
-                <img
-                  src={product.image}
-                  alt={product.title}
-                  className="img-fluid"
-                />
-              </div>
-              <div className="col-md-6 pt-5">
-                <h4 className="text-uppercase text-warning ">
-                  {product.category}
-                </h4>
-                <hr />
-                <h4 className=" text-black-50 ">{product.title}</h4>
-                <hr />
-                <p className="lead fw-bold text-black-50  ">
-                  Rating {product.rating && product.rating.rate}
-                  <i className="fa fa-star text-secondary"></i>
-                </p>
-                <hr />
-                <h3 className="display-7 fw-bold text-black-50 ">
-                  $ {product.price}
-                </h3>
-                <hr />
-                <p className="lead fw-bold text-black-50  ">
-                  {product.description}
-                </p>
-                <button className="btn btn-outline-dark ms-2 px-3 py-3   ">
-                  Add to Cart
-                </button>
-                <NavLink
-                  to="/cart"
-                  className="btn btn-outline-dark ms-2 px-3 py-3  "
-                >
-                  Go to Cart
-                </NavLink>
-              </div>
-            </>
-          )}
+    <div className="container py-5">
+      <div className="row py-3">
+        <div className="col-md-6 pt-5">
+          <img src={product.image} alt={product.title} className="img-fluid" />
+        </div>
+        <div className="col-md-6 pt-5">
+          <h4 className="text-uppercase text-warning ">{product.category}</h4>
+          <hr />
+          <h4 className=" text-black-50 ">{product.title}</h4>
+          <hr />
+          <p className="lead fw-bold text-black-50  ">
+            Rating {product.rating && product.rating.rate}
+            <i className="fa fa-star text-secondary"></i>
+          </p>
+          <hr />
+          <h3 className="display-7 fw-bold text-black-50 ">
+            $ {product.price}
+          </h3>
+          <hr />
+          <p className="lead fw-bold text-black-50  ">{product.description}</p>
+          <button className="btn btn-outline-dark ms-2 px-3 py-3   ">
+            Add to Cart
+          </button>
+          <NavLink to="/cart" className="btn btn-outline-dark ms-2 px-3 py-3  ">
+            Go to Cart
+          </NavLink>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
-export default ProductPage;
+export default ProductCard;
