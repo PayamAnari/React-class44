cdbb1e6652bb135d1d817e499bf4e1217567ba59
PayamAnari <anari.p62@gmail.com>
2023-10-03 23:28:55 +0200

    correction

--- ecommerce/src/index.js
+++
@@ -1,8 +0,0 @@
-// index.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/font-awesome/css/font-awesome.min.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
